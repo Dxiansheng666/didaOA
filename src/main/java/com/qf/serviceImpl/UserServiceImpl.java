d36388cee08a1b0f4c859cee3a066124f463e8a7
astoundedr <857604104@qq.com>
2019-11-11 15:41:37 +0800

    UserServiceImpl

--- src/main/java/com/qf/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/qf/serviceImpl/UserServiceImpl.java
@@ -1,8 +1,28 @@
 package com.qf.serviceImpl;
 
+import com.qf.mapper.UserMapper;
+import com.qf.pojo.User;
 import com.qf.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public String UserLogin(User user) {
+        return userMapper.UserLogin(user);
+    }
+
+    @Override
+    public String getUpwdByUname(String uname) {
+        return userMapper.getUpwdByUname(uname);
+    }
+
+    @Override
+    public String getRoleByUname(String uname) {
+        return userMapper.getRolenameByUname(uname);
+    }
 }
