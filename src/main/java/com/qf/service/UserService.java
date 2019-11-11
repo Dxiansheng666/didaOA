5469ea9652e67f4018ad2ad6a35156fb4d145d4e
astoundedr <857604104@qq.com>
2019-11-11 15:41:08 +0800

    UserService

--- src/main/java/com/qf/service/UserService.java
+++ src/main/java/com/qf/service/UserService.java
@@ -1,4 +1,12 @@
 package com.qf.service;
 
+import com.qf.pojo.User;
+
 public interface UserService {
+    //登录验证
+    public String UserLogin(User user);
+    //通过名字得到密码
+    public String getUpwdByUname(String uname);
+    //通过名字得到角色
+    public String getRoleByUname(String uname);
 }
