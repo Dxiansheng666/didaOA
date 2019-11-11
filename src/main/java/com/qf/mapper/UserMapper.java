4f595382052d8a73aa55a5cc08ea34343a3de533
astoundedr <857604104@qq.com>
2019-11-11 16:11:02 +0800

    UserMapper

--- src/main/java/com/qf/mapper/UserMapper.java
+++ src/main/java/com/qf/mapper/UserMapper.java
@@ -8,10 +8,11 @@ import com.qf.pojo.User;
  * @Time 14:15
  */
 public interface UserMapper {
-    //登录验证
-    public String UserLogin(User user);
+
     //通过名字得到密码
     public String getUpwdByUname(String uname);
     //通过名字得到角色
     public String getRolenameByUname(String uname);
+    //通过名字得到对象信息
+    public User getUserByUname(String uname);
 }
