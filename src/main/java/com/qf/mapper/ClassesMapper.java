0893fdcfc71bfeb25d25b16a1ed4a463402fea79
astoundedr <857604104@qq.com>
2019-11-12 10:50:48 +0800

    fjm

---
+++ src/main/java/com/qf/mapper/ClassesMapper.java
@@ -0,0 +1,25 @@
+package com.qf.mapper;
+
+import com.qf.pojo.Classes;
+
+import java.util.List;
+
+/**
+ * @author FJM
+ * @create 2019/11/12
+ * @Time 9:52
+ * 管理员对班级进行管理
+ */
+public interface ClassesMapper {
+    //添加班级及讲师和班主任
+    public int addClasses(Classes classes);
+    //删除班级信息
+    public int deleteClasses(int class_id);
+    //通过id查询班级信息
+    public Classes getClassesById(int class_id);
+    //修改班级信息
+    public int updateClasses(Classes classes);
+    //查询所有班级信息
+    public List<Classes> classesList();
+
+}
