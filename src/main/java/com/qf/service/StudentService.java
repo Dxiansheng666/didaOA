962337c40292096dc0f2d7339173fd3e72c2e8d9
4AMwe1less <1208348645@qq.com>
2019-11-12 17:32:57 +0800

    student修改12

--- src/main/java/com/qf/service/StudentService.java
+++ src/main/java/com/qf/service/StudentService.java
@@ -1,10 +1,8 @@
 package com.qf.service;
 
-import com.qf.pojo.Student;
-import com.qf.pojo.Student_Holiday;
-import com.qf.pojo.User;
-import com.qf.pojo.WeekReport;
+import com.qf.pojo.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 public interface StudentService {
@@ -13,7 +11,7 @@ public interface StudentService {
     //修改个人资料
     public Student updateStudentInfo(int sid);
     //修改密码
-    public int updateUpwd(User user);
+    public int updateUpwd(int sid);
     //新增周报
     public int addWeekReport(int sid);
     //删除周报
@@ -21,11 +19,13 @@ public interface StudentService {
     //查询周报
     public List<WeekReport> getWeekReportList(int sid);
     //发起请假
-    public int addHoliday(Student_Holiday student_holiday);
+    public int addHoliday(Student_Holiday student_holiday, HttpSession session);
     //根据学生查找老师
-    public String getTeacherName(Student student);
+    public List<Classes> getClassBySid(int sid);
     //查询周报分数
     public List<WeekReport> getWeekReportScoreList(int sid);
     //查找角色集合
     public List<User> getRoleNameList();
+    //通过uid查询sid
+    public int getSidByUid(int uid);
 }
