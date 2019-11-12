9d6a01c28461c11960656de8b092819bcbc88dce
4AMwe1less <1208348645@qq.com>
2019-11-12 17:32:01 +0800

    student修改11

--- src/main/java/com/qf/mapper/StudentMapper.java
+++ src/main/java/com/qf/mapper/StudentMapper.java
@@ -1,8 +1,5 @@
 package com.qf.mapper;
-import com.qf.pojo.Student;
-import com.qf.pojo.Student_Holiday;
-import com.qf.pojo.User;
-import com.qf.pojo.WeekReport;
+import com.qf.pojo.*;
 
 import java.util.List;
 
@@ -23,7 +20,7 @@ public interface StudentMapper {
     //修改个人资料
     public Student updateStudentInfo(int sid);
     //修改密码
-    public int updateUpwd(User user);
+    public int updateUpwd(int user);
     //新增周报
     public int addWeekReport(int sid);
     //删除周报
@@ -33,7 +30,7 @@ public interface StudentMapper {
     //发起请假
     public int addHoliday(Student_Holiday student_holiday);
     //根据学生查找老师
-    public String getTeacherName(Student student);
+    public List<Classes> getClassBySid(int sid);
     //查询周报分数
     public List<WeekReport> getWeekReportScoreList(int sid);
     //查找角色集合
