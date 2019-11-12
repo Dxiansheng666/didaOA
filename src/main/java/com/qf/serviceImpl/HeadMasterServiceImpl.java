003f21a0a3c3b613adb7ac73c3d553a7f292889a
Dxiansheng666 <2838928386@qq.com>
2019-11-12 17:47:10 +0800

    bpmn

--- src/main/java/com/qf/serviceImpl/HeadMasterServiceImpl.java
+++ src/main/java/com/qf/serviceImpl/HeadMasterServiceImpl.java
@@ -1,16 +1,20 @@
 package com.qf.serviceImpl;
 
-import com.qf.mapper.EmployeeMapper;
-import com.qf.mapper.StudentMapper;
-import com.qf.mapper.UserMapper;
-import com.qf.mapper.WeekReportMapper;
+import com.qf.mapper.*;
 import com.qf.pojo.*;
 import com.qf.service.HeadMasterService;
 import com.qf.util.MD5Utils;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 @Service
 public class HeadMasterServiceImpl implements HeadMasterService {
     @Autowired
@@ -21,6 +25,14 @@ public class HeadMasterServiceImpl implements HeadMasterService {
     private WeekReportMapper weekReportMapper;
     @Autowired
     private StudentMapper studentMapper;
+    @Autowired
+    private StudentHolidayMapper studentHolidayMapper;
+    @Autowired
+    private EmployeeHolidayMapper employeeHolidayMapper;
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private TaskService taskService;
     //获取个人资料
     @Override
     public Employee getHeadMasterByUid(int uid) {
@@ -33,8 +45,8 @@ public class HeadMasterServiceImpl implements HeadMasterService {
     }
     //获取学生周报信息
     @Override
-    public List<WeekReport> getWeekReportListBySid(int sid) {
-        return weekReportMapper.getWeekReportListBySid(sid);
+    public List<WeekReport> getWeekReportListByUid(int uid) {
+        return weekReportMapper.getWeekReportListByUid(uid);
     }
     //获取班级学生信息
     @Override
@@ -44,7 +56,26 @@ public class HeadMasterServiceImpl implements HeadMasterService {
     //班主任发起请假
     @Override
     public int addHeadMasterHoliday(Employee_Holiday employee_holiday) {
-        return 0;
+        int i = employeeHolidayMapper.addHoliday(employee_holiday);
+        /**
+         * 启动流程
+         * startProcessInstanceByKey(String,String,map)
+         * 参数说明：
+         * 第一个string表示流程的key
+         * 第二个string表示业务的key，即bussiness key，往往业务表的主键值
+         * 第3个map表示流程变量
+         */
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("tname",employee_holiday.getEmployee().getEname());
+        map.put("bname","王");
+        int days = getDays(employee_holiday.getStart_date(),employee_holiday.getEnd_date());
+        map.put("days",days);
+        //发起流程实例
+        runtimeService.startProcessInstanceByKey("employee_holiday",employee_holiday.getHid()+"",map);
+        //完成任务
+        Task task = taskService.createTaskQuery().taskAssignee(employee_holiday.getEmployee().getEname()).singleResult();
+        taskService.complete(task.getId());
+        return employee_holiday.getHid();
     }
     //查看班级成绩
     @Override
@@ -58,7 +89,43 @@ public class HeadMasterServiceImpl implements HeadMasterService {
     }
     //审批学生请假
     @Override
-    public int updateStudentHoliday() {
-        return 0;
+    public int updateStudentHoliday(int hid,int state,String ename) {
+        Task task = taskService.createTaskQuery().processInstanceBusinessKey(hid + "").taskAssignee(ename).singleResult();
+        taskService.complete(task.getId());
+        return studentHolidayMapper.updateStudentHoliday(hid,state);
+    }
+    //获取待审批假条
+    @Override
+    public List<Student_Holiday> getStudentHolidayByUid(int uid) {
+        Employee headMasterByUid = employeeMapper.getHeadMasterByUid(uid);
+        List<Task> list = taskService.createTaskQuery().taskAssignee(headMasterByUid.getEname()).list();
+        List<String> bussinessKeys = new ArrayList<String>();
+        for (Task task : list) {
+            ProcessInstance processInstance = runtimeService.createProcessInstanceQuery()
+                    .processInstanceId(task.getProcessInstanceId()).singleResult();
+            bussinessKeys.add(processInstance.getBusinessKey());
+        }
+        List<Student_Holiday> approveHolidayList = studentHolidayMapper.getApproveHolidayList(bussinessKeys);
+        return approveHolidayList;
+    }
+    /**
+     * 根据日期获取天数
+     * @param startDate
+     * @param endDate
+     * @return
+     */
+    private int getDays(String startDate, String endDate) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date start = null;
+        Date end = null;
+        try {
+            start = simpleDateFormat.parse(startDate);
+            end = simpleDateFormat.parse(endDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        long daysTime = end.getTime() - start.getTime();
+        int days = (int)daysTime/(24*60*60*1000);
+        return days;
     }
 }
